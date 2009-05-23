925e70080f4b22120a699c8678a3a191623fae6f
Dmitri Babaev <dmitri.babaev@gmail.com>
2009-05-23 20:33:11 +0400

    more functors refactoring

--- src/main/java/org/ext/jft/function/Mapper.java
+++
@@ -1,9 +0,0 @@
-package org.ext.jft.function;
-
-/**
- * Transforms From object into To object
- * @author Dmitri Babaev
- */
-public interface Mapper<From, To> {
-	To map(From from);
-}
--- src/main/java/org/ext/jft/function/Predicate.java
+++
@@ -1,8 +0,0 @@
-package org.ext.jft.function;
-
-/**
- * @author Dmitri Babaev
- */
-public interface Predicate<T> {
-	public boolean test(T val);
-}
