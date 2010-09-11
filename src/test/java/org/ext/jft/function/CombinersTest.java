6be2c249f6e1a256b5fa408e431f9419db79a149
dmitri <devnull@localhost>
2010-09-11 20:23:52 +0400

    min and max combiners

---
+++ src/test/java/org/ext/jft/function/CombinersTest.java
@@ -0,0 +1,19 @@
+package org.ext.jft.function;
+
+import static org.ext.jft.container.Containers.arrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CombinersTest {
+	
+	@Test
+	public void maxCombiner() {
+		Assert.assertEquals(4, arrayList(1, 2, 3, 4, 3).reduce(Combiners.<Integer>max(), Integer.MIN_VALUE).intValue());
+	}
+	
+	@Test
+	public void minCombiner() {
+		Assert.assertEquals(1, arrayList(2, 3, 1, 4, 3).reduce(Combiners.<Integer>min(), Integer.MAX_VALUE).intValue());
+	}
+}
