f7c83b3727ffa103b8fb72c41a5748337037ec3d
dmitri <devnull@localhost>
2010-09-24 01:37:36 +0400

    Cast is renamed to AnyType RegExp unit test

--- src/main/java/org/ext/jft/tool/RegExp.java
+++ src/main/java/org/ext/jft/tool/RegExp.java
@@ -9,17 +9,15 @@ import org.ext.jft.container.Option;
  * @author Dmitri Babaev
  */
 public class RegExp {
-	public static Option<String> findFirstGroup(Pattern pattern, String input) {
+	public static Option<String> find(Pattern pattern, String input) {
 		Matcher m = pattern.matcher(input);
 		if (!m.find())
 			return Option.none();
-		else if (m.groupCount() < 1)
-			return Option.none();
 		else
-			return Option.some(m.group(1));
+			return Option.some(m.group());
 	}
 
-	public static Option<String> findFirstGroup(String pattern, String input) {
-		return findFirstGroup(Pattern.compile(pattern), input);
+	public static Option<String> find(String pattern, String input) {
+		return find(Pattern.compile(pattern), input);
 	}
 }
